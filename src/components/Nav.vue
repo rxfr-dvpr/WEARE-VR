3077867674ec96b3f20828d32741b72a4df7a2a8
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-03-22 23:49:03 +0500

    navigation updated

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -12,20 +12,26 @@
                 <button class="nav-contact-btn all-btn">получить  консультацию</button>
             </div>
             
-            <button class="nav-btn">
+            <button class="nav-btn" @click="linksOpened = !linksOpened">
                 <span class="nav-btn-line"></span>
                 <span class="nav-btn-line scnd"></span>
 
-                <span class="nav-btn-txt">закрыть</span>
+                <span class="nav-btn-txt" :class="{'opened': linksOpened}">закрыть</span>
             </button>
 
-            <div class="nav-wrapper">
-                <ul class="nav__list">
-                    <li class="nav__list-item">
-                        <a href="" class="nav__list-link"></a>
-                    </li>
-                </ul>
-            </div>
+            <Transition name="wrapper">
+                <div class="nav-wrapper" v-show="linksOpened">
+                    <ul class="nav__list">
+                        <li class="nav__list-item">
+                            <a href="" class="nav__list-link"></a>
+                        </li>
+                    </ul>
+
+                    <img :src="bgImg" alt="" class="bg-img">
+
+                    <img :src="bgVector" alt="" class="bg-vector">
+                </div>
+            </Transition>
 
         </div>
     </div>
@@ -35,7 +41,14 @@
 <script>
 
 export default {
-    name: 'Navigation'
+    name: 'Navigation',
+    data() {
+        return {
+            bgImg: "https://firebasestorage.googleapis.com/v0/b/weare-vr.appspot.com/o/bg-filter.png?alt=media&token=54c9f47c-6cb6-44c8-a113-f4351b4ab231",
+            bgVector: 'https://firebasestorage.googleapis.com/v0/b/weare-vr.appspot.com/o/bg-vector.svg?alt=media&token=84c62bf9-00de-4499-8690-167f0bb8e7fd',
+            linksOpened: false
+        }
+    }
 }
 
 </script>
@@ -54,6 +67,7 @@ export default {
     &-logo {
         max-width: 200px;
         width: 100%;
+        z-index: 6;
 
         &-img {
             width: 100%;
@@ -94,6 +108,7 @@ export default {
         justify-content: center;
         border: solid 2px var(--main-blue);
         border-radius: 50%;
+        z-index: 6;
 
         &-line {
             width: 27px;
@@ -118,12 +133,55 @@ export default {
             text-transform: uppercase;
             opacity: 0;
             pointer-events: none;
+            transition: .4s;
+
+            &.opened {
+                opacity: 1;
+                pointer-events: all !important;
+            }
         }
     }
 
     &-wrapper {
         position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background: rgb(32,59,124);
+        background: linear-gradient(145deg, rgba(32,59,124,1) 0%, rgba(73,47,116,1) 100%);
+        overflow: hidden;
+        z-index: 5;
+
+        .bg-img {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            opacity: .5;
+            pointer-events: none;
+        }
+
+        .bg-vector {
+            max-width: 835px;
+            width: 100%;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            opacity: .1;
+            pointer-events: none;
+        }
     }
+}
+
+.wrapper-enter-active,
+.wrapper-leave-active {
+  transition: .4s ease;
+}
+
+.wrapper-enter-from,
+.wrapper-leave-to {
+  opacity: 0;
 }
 
 </style>
