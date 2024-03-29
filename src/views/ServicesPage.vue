8e422d8e3cd7769268754ae312b7b3e215c95656
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-03-29 18:45:47 +0500

    cards added to services page

--- src/views/ServicesPage.vue
+++ src/views/ServicesPage.vue
@@ -9,6 +9,16 @@
 
           <p class="services__descr-txt">{{ txt }}</p>
         </div>
+
+        <div class="services__list">
+          <figure class="services__list-item" v-for="(item, idx) in cards" :key="idx">
+            <img :src="item.img" alt="" class="service-img">
+
+            <figcaption class="service-name">{{ item.title }}</figcaption>
+
+            <router-link :to="`/services`" class="service-link">Подробнее <i class="fal fa-long-arrow-right"></i></router-link>
+          </figure>
+        </div>
         
       </div>
     </div>
@@ -23,7 +33,17 @@ export default {
   data() {
     return {
       title: 'Комплексные решения визуализации',
-      txt: 'WEARE – российский разработчик программного обеспечения и массогабаритного оборудования, предоставляющий максимально полное погружение в виртуальную реальность. '
+      txt: 'WEARE – российский разработчик программного обеспечения и массогабаритного оборудования, предоставляющий максимально полное погружение в виртуальную реальность.',
+      cards: [
+        {
+          title: 'Разработка VR решений',
+          img: 'https://firebasestorage.googleapis.com/v0/b/weare-vr.appspot.com/o/Services%2Fservices-img-1.png?alt=media&token=785d8602-4c18-457b-a3a0-a48de2b05b6f'
+        },
+        {
+          title: 'Дополненная реальность для Вашего бизнеса',
+          img: 'https://firebasestorage.googleapis.com/v0/b/weare-vr.appspot.com/o/Services%2Fservices-img-2.png?alt=media&token=40345fdd-829c-4cbb-8dd6-158eba779452'
+        }
+      ]
     }
   }
 }
@@ -34,7 +54,12 @@ export default {
 
 .services__section {
   width: 100%;
-  padding-top: 70px;
+  padding-top: 50px;
+
+  .row {
+    flex-direction: column;
+    row-gap: 50px;
+  }
 
   .services__descr {
     max-width: 1190px;
@@ -56,6 +81,52 @@ export default {
       width: 100%;
       font-size: 17px;
       color: rgba($color: #fff, $alpha: .8);
+    }
+  }
+
+  .services__list {
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+
+    &-item {
+      max-width: 540px;
+      width: 100%;
+      border: solid 1px rgba($color: #fff, $alpha: .1);
+      border-radius: 3px;
+      overflow: hidden;
+      background: #000032;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding-bottom: 33px;
+      row-gap: 10px;
+      text-align: center;
+      cursor: pointer;
+      height: max-content;
+
+      .service-img {
+        width: 100%;
+        object-fit: contain;
+        max-height: 320px;
+      }
+
+      .service-name {
+        max-width: 300px;
+        width: 100%;
+        font-size: 22px;
+        font-weight: 500;
+      }
+
+      .service-link {
+        font-size: 14px;
+        text-transform: uppercase;
+        letter-spacing: 1.2px;
+      }
+
+      &:last-of-type {
+        margin-top: 40px;
+      }
     }
   }
 }
