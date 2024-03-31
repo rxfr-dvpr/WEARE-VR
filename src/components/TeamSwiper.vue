f355f9f27b92c47353644b280afb8b10ad3ede6e
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-03-31 22:35:57 +0500

    slider added to teams page

---
+++ src/components/TeamSwiper.vue
@@ -0,0 +1,137 @@
+<template>
+  <swiper :modules="modules" class="team-swiper" :slidesPerView="4.5" :spaceBetween="35" :navigation="cltrs">
+    <swiper-slide class="team-swiper-slide" v-for="(slide, idx) in slides" :key="idx">
+        <div class="slide-box">
+            <img :src="slide.img" alt="" class="slide-img">
+
+            <p class="slide-name">{{ slide.name }}</p>
+        </div>
+
+        <span class="slide-job">{{ slide.job }}</span>
+    </swiper-slide>
+  </swiper>
+
+  <div class="swiper-ctrls">
+    <span class="swiper-ctrls-btn left"><i class="fal fa-long-arrow-left"></i></span>
+    <span class="swiper-ctrls-btn right"><i class="fal fa-long-arrow-right"></i></span>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import { Navigation } from 'swiper/modules';
+
+export default {
+    name: 'Team Swiper',
+    props: {
+        slides: {
+            type: Array,
+            default: [],
+            required: true
+        }
+    },
+    components: {
+        Swiper,
+        SwiperSlide
+    },
+    data() {
+        return {
+            modules: [Navigation],
+            cltrs: {
+                prevEl: '.swiper-ctrls-btn.left',
+                nextEl: '.swiper-ctrls-btn.right'
+            }
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+.team-swiper {
+    width: 100%;
+
+    &-slide {
+        max-width: 300px;
+        width: 100%;
+        display: flex;
+        gap: 10px;
+
+        .slide-job {
+            font-size: 15px;
+            color: rgba($color: #fff, $alpha: .6);
+            writing-mode: vertical-rl;
+            text-orientation: mixed;
+            font-weight: 300;
+            margin-top: 15px;
+        }
+
+        .slide-box {
+            max-width: 270px;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            row-gap: 12px;
+            
+            .slide-img {
+                width: 100%;
+                max-height: 390px;
+                height: 100%;
+                object-fit: cover;
+                overflow: hidden;
+            }
+            
+            .slide-name {
+                font-size: 20px;
+                text-transform: capitalize;
+                font-weight: 300;
+            }
+        }
+
+        &:nth-child(2n) {
+            margin-top: 45px;
+        }
+    }
+}
+
+.swiper-ctrls {
+    max-width: 180px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    border: solid 1px rgba($color: #fff, $alpha: .1);
+    border-radius: 65px;
+    margin-left: auto;
+
+    &-btn {
+        font-size: 25px;
+        cursor: pointer;
+
+        i {
+            transition: .3s;
+        }
+
+        &:hover i {
+            color: var(--main-blue);
+        }
+
+        &.swiper-button-disabled {
+            cursor: default;
+
+            i {
+                opacity: .3;
+            }
+
+            &:hover i {
+                color: var(--primary-color);
+            }
+        }
+    }
+}
+
+</style>
--- src/views/TeamPage.vue
+++ src/views/TeamPage.vue
@@ -3,12 +3,15 @@
     <div class="container">
       <div class="row">
         <h2 class="team-title">{{ title }}</h2>
+
+        <TeamSwiper :slides="slides"/>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import TeamSwiper from '@/components/TeamSwiper.vue'
 
 export default {
   name: 'Team Page',
@@ -19,25 +22,59 @@ export default {
         {
           name: 'Денис Петров',
           job: 'Дизайнер интерьеров',
-          img: ''
+          img: 'https://firebasestorage.googleapis.com/v0/b/weare-vr.appspot.com/o/Team%2Fdenis.png?alt=media&token=f30d44a4-65d2-402f-9454-17332c686350'
         },
         {
           name: 'Елезовета Туктамышева',
           job: 'Дизайнер интерьеров',
-          img: ''
+          img: 'https://firebasestorage.googleapis.com/v0/b/weare-vr.appspot.com/o/Team%2Felezoveta.png?alt=media&token=a644a918-6741-4d09-b712-609e8340ef21'
         },
         {
           name: 'Евдоким Кляров',
           job: 'Дизайнер интерьеров',
-          img: ''
+          img: 'https://firebasestorage.googleapis.com/v0/b/weare-vr.appspot.com/o/Team%2Fevdokim.png?alt=media&token=e330a67c-60ba-4a24-976d-6ed2ff671836'
+        },
+        {
+          name: 'Денис Петров',
+          job: 'Дизайнер интерьеров',
+          img: 'https://firebasestorage.googleapis.com/v0/b/weare-vr.appspot.com/o/Team%2Fdenis.png?alt=media&token=f30d44a4-65d2-402f-9454-17332c686350'
+        },
+        {
+          name: 'Елезовета Туктамышева',
+          job: 'Дизайнер интерьеров',
+          img: 'https://firebasestorage.googleapis.com/v0/b/weare-vr.appspot.com/o/Team%2Felezoveta.png?alt=media&token=a644a918-6741-4d09-b712-609e8340ef21'
+        },
+        {
+          name: 'Евдоким Кляров',
+          job: 'Дизайнер интерьеров',
+          img: 'https://firebasestorage.googleapis.com/v0/b/weare-vr.appspot.com/o/Team%2Fevdokim.png?alt=media&token=e330a67c-60ba-4a24-976d-6ed2ff671836'
         }
       ]
     }
+  },
+  components: {
+    TeamSwiper
   }
 }
 
 </script>
 
 <style lang="scss" scoped>
+
+.team__section {
+  width: 100%;
+  padding-top: 70px;
+
+  .row {
+    flex-direction: column;
+    row-gap: 40px;
+  }
+
+  .team-title {
+    font-size: 48px;
+    font-weight: 600;
+    text-transform: uppercase;
+  }
+}
 
 </style>
