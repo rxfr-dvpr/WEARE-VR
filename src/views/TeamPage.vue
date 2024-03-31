d0f4f34dd984b88f7dc1f8bedf674d476731eedd
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-03-31 21:11:50 +0500

    swiper added | team page dev started

--- src/views/TeamPage.vue
+++ src/views/TeamPage.vue
@@ -1,11 +1,39 @@
 <template>
-  <h1>Team Page</h1>
+  <section class="team__section">
+    <div class="container">
+      <div class="row">
+        <h2 class="team-title">{{ title }}</h2>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 
 export default {
-  name: 'Team Page'
+  name: 'Team Page',
+  data() {
+    return {
+      title: 'Наша команда',
+      slides: [
+        {
+          name: 'Денис Петров',
+          job: 'Дизайнер интерьеров',
+          img: ''
+        },
+        {
+          name: 'Елезовета Туктамышева',
+          job: 'Дизайнер интерьеров',
+          img: ''
+        },
+        {
+          name: 'Евдоким Кляров',
+          job: 'Дизайнер интерьеров',
+          img: ''
+        }
+      ]
+    }
+  }
 }
 
 </script>
