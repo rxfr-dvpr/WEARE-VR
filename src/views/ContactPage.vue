62c5575e17cc9a77eb1b52fef058d0c2c5e2ed47
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-04-02 22:50:07 +0500

    contact form updated in contact page

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -7,10 +7,16 @@
           <h4 class="contact__form-title">{{ form.title }}</h4>
 
           <div class="form-inputs">
-            <input :type="input.type" v-for="(input, idx) in form.inputs" :key="idx"
-            :class="`form-inputs-item ${input.type}-inp`" v-model="input.val">
+            <input :type="input.type" v-for="(input, idx) in form.inputs" :key="idx" required
+            :class="`form-inputs-item ${input.type}-inp`" v-model="input.val" :placeholder="input.plc">
 
-            <textarea class="form-inputs-item" v-model="form.msg"></textarea>
+            <textarea class="form-inputs-item msg-inp" rows="1" placeholder="Сообщение" required v-model="form.msg"></textarea>
+          </div>
+
+          <div class="form-ctrls">
+            <label class="form-policy"><input type="checkbox" required> {{ form.policy }}</label>
+
+            <button class="form-btn all-btn" type="submit">{{ form.btn }}</button>
           </div>
         </form>
 
@@ -35,19 +41,22 @@ export default {
         inputs: [
           {
             type: 'text',
-            val: ''
+            val: '',
+            plc: 'Имя'
           },
           {
             type: 'email',
-            val: ''
+            val: '',
+            plc: 'E-mail'
           },
           {
             type: 'number',
-            val: ''
+            val: '',
+            plc: 'Телефон'
           }
         ],
         msg: '',
-        btnVal: '',
+        btn: 'Отправить заявку',
         policy: 'Я согласен с политикой конфиденциальности'
       }
     }
@@ -82,6 +91,60 @@ export default {
       
       &::first-letter {
         text-transform: uppercase;
+      }
+    }
+
+    .form-inputs {
+      width: 100%;
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      justify-content: space-between;
+      row-gap: 30px;
+
+      &-item {
+        max-width: 270px;
+        width: 100%;
+        background: transparent;
+        border: 0;
+        border-bottom: 1px solid rgba($color: #fff, $alpha: .4);
+        padding: 15px 0 15px 10px;
+        font-size: 15px;
+
+        &::placeholder {
+          color: var(--primary-color);
+          opacity: .9;
+        }
+
+        &.msg-inp {
+          resize: vertical;
+        }
+      }
+    }
+
+    .form-ctrls {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      column-gap: 25px;
+      row-gap: 20px;
+      flex-wrap: wrap;
+
+      .form-btn {
+        max-width: 240px;
+        width: 100%;
+        background: var(--main-blue);
+        border-color: var(--main-blue);
+      }
+
+      .form-policy {
+        max-width: 210px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        gap: 15px;
+        font-size: 14px;
+        cursor: pointer;
+        user-select: none
       }
     }
   }
